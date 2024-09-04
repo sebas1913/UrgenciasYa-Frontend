cf5f396db78616d5208be74ab3756804ab41be49
sebas1913 <sebasosornicastrillon@gmail.com>
2024-09-04 10:35:01 -0500

    Update comments for Button component

--- src/app/components/UI/button/Button.tsx
+++ src/app/components/UI/button/Button.tsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import { MouseEventHandler } from "react";
 
-//Create i
+//creation of the interface for the atomic input component
 export interface ButtonProps {
   type: "submit" | "button" | "reset";
   label: string;
@@ -10,6 +10,7 @@ export interface ButtonProps {
   className?: string;
 }
 
+//Component creation
 const Button: React.FC<ButtonProps> = ({ label, onClick, className, type }) => {
   return (
     <button type={type} onClick={onClick} className={className}>
