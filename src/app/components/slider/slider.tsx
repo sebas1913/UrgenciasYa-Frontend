7d9d635ce8ba9556c93e77aaa4fe1109598c60dd
sebas1913 <sebasosornicastrillon@gmail.com>
2024-09-07 19:07:00 -0500

    Slider refactoring: change from styled-components to SCSS

--- src/app/components/slider/slider.tsx
+++ src/app/components/slider/slider.tsx
@@ -1,58 +1,14 @@
+
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 import { Autoplay, Pagination, Navigation } from 'swiper/modules';
-import styled from 'styled-components';
-
-const SwiperContainer = styled.div`
-  width: 70%;
-  margin: 0 auto;
-  height: 500px;
-  position: relative;
-
-  .swiper-button-next,
-  .swiper-button-prev {
-    display: none; 
-  }
-`;
-
-const CardContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background: #fff;
-  border-radius: 8px;
-  box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.2);
-  padding: 10px;
-  box-sizing: border-box;
-  width: 100%;
-  height: 200px;
-  margin: 40px 0px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-const CardLink = styled.a`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 70%;
-  height: 100%;
-  text-decoration: none; 
-`;
-
-const SliderImage = styled.img`
-  max-width: 100%;
-  max-height: 100%;
-  object-fit: contain;
-  border-radius: 8px;
-`;
+import styles from './slider.module.scss'; 
 
 const Slider = () => {
   return (
-    <SwiperContainer>
+    <div className={styles.swiperContainer}>
       <Swiper
         spaceBetween={30}
         slidesPerView={3}
@@ -64,54 +20,53 @@ const Slider = () => {
         pagination={{
           clickable: true,
         }}
-        navigation={true}
+        navigation={false}
         modules={[Autoplay, Pagination, Navigation]}
-        className="mySwiper"
       >
         <SwiperSlide>
-          <CardContainer>
-            <CardLink href="https://www.epssura.com/" title='Sitio web Sura' target="_blank" rel="noopener noreferrer">
-                <SliderImage src="/images/sura.png" alt="Logo Sura" />
-            </CardLink>
-          </CardContainer>
+          <div className={styles.cardContainer}>
+            <a className={styles.cardLink} href="https://www.epssura.com/" title='Sitio web Sura' target="_blank" rel="noopener noreferrer">
+                <img className={styles.sliderImage} src="/images/sura.png" alt="Logo Sura" />
+            </a>
+          </div>
         </SwiperSlide>
         <SwiperSlide>
-          <CardContainer>
-            <CardLink href="https://www.mutualser.com/" title='Sitio web Mutualser' target="_blank" rel="noopener noreferrer">
-                <SliderImage src="/images/mutualSer.png" alt="Logo MutualSer" />
-            </CardLink>
-          </CardContainer>
+          <div className={styles.cardContainer}>
+            <a className={styles.cardLink} href="https://www.mutualser.com/" title='Sitio web Mutualser' target="_blank" rel="noopener noreferrer">
+                <img className={styles.sliderImage} src="/images/mutualSer.png" alt="Logo MutualSer" />
+            </a>
+          </div>
         </SwiperSlide>
         <SwiperSlide>
-          <CardContainer>
-            <CardLink href="https://www.nuevaeps.com.co/" title='Sitio web Nueva EPS' target="_blank" rel="noopener noreferrer">
-                <SliderImage src="/images/nuevaEps.png" alt="Logo NuevaEPS" />
-            </CardLink>
-          </CardContainer>
+          <div className={styles.cardContainer}>
+            <a className={styles.cardLink} href="https://www.nuevaeps.com.co/" title='Sitio web Nueva EPS' target="_blank" rel="noopener noreferrer">
+                <img className={styles.sliderImage} src="/images/nuevaEps.png" alt="Logo NuevaEPS" />
+            </a>
+          </div>
         </SwiperSlide>
         <SwiperSlide>
-          <CardContainer>
-            <CardLink href="https://www.epssanitas.com/" title='Sitio web EPS Sanitas' target="_blank" rel="noopener noreferrer">
-                <SliderImage src="/images/sanitas.png" alt="Logo Sanitas" />
-            </CardLink>
-          </CardContainer>
+          <div className={styles.cardContainer}>
+            <a className={styles.cardLink} href="https://www.epssanitas.com/" title='Sitio web EPS Sanitas' target="_blank" rel="noopener noreferrer">
+                <img className={styles.sliderImage} src="/images/sanitas.png" alt="Logo Sanitas" />
+            </a>
+          </div>
         </SwiperSlide>
         <SwiperSlide>
-          <CardContainer>
-            <CardLink href="https://www.saviasaludeps.com/" title='Sitio web Savia Salud' target="_blank" rel="noopener noreferrer">
-                <SliderImage src="/images/saviaSalud.png" alt="Logo SaviaSalud" />
-            </CardLink>
-          </CardContainer>
+          <div className={styles.cardContainer}>
+            <a className={styles.cardLink} href="https://www.saviasaludeps.com/" title='Sitio web Savia Salud' target="_blank" rel="noopener noreferrer">
+                <img className={styles.sliderImage} src="/images/saviaSalud.png" alt="Logo SaviaSalud" />
+            </a>
+          </div>
         </SwiperSlide>
         <SwiperSlide>
-          <CardContainer>
-            <CardLink href="https://www.sisben.gov.co/" title='Sitio web Sisben' target="_blank" rel="noopener noreferrer">
-                <SliderImage src="/images/sisben.png" alt="Logo Sisben" />
-            </CardLink>
-          </CardContainer>
+          <div className={styles.cardContainer}>
+            <a className={styles.cardLink} href="https://www.sisben.gov.co/" title='Sitio web Sisben' target="_blank" rel="noopener noreferrer">
+                <img className={styles.sliderImage} src="/images/sisben.png" alt="Logo Sisben" />
+            </a>
+          </div>
         </SwiperSlide>
       </Swiper>
-    </SwiperContainer>
+    </div>
   );
 };
 
