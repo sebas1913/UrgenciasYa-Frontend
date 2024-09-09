b662cc161a3520db461ca3e69c8a96ddf62c10f1
Sebastian <sebastianroldangiraldo@gmail.com>
2024-09-09 13:17:50 -0500

    create home information component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,15 @@
 import Slider from "./components/slider/slider";
 import AboutUs from "./components/about-us/About-us";
 import SearchForm from "./components/search-form/Search-form";
+import HomeInformation from "./components/home-information/Home-information";
+
 export default function Home() {
   return (
     <main>
+        <HomeInformation></HomeInformation>
         <SearchForm></SearchForm>
         <Slider></Slider>
         <AboutUs></AboutUs>
-
     </main>
   );
 }
