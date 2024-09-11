c55e3e3a279bfec4e3bcfbeec22ebfb638fea85a
Sebastian <sebastianroldangiraldo@gmail.com>
2024-09-11 08:26:33 -0500

    refactor navbar component code

--- src/app/components/navbar/Navbar.tsx
+++ src/app/components/navbar/Navbar.tsx
@@ -7,14 +7,23 @@ import RegisterForm from '../register-form/Register-form';
 // import FormularioLogin from '../FormularioLogin'; 
 
 const Navbar: React.FC = () => {
-  const [showForm, setShowForm] = useState(false);
+  const [showRegisterForm, setShowRegisterForm] = useState(false);
+  const [showLoginForm, setShowLoginForm] = useState(false);
 
-  const handleToggleForm = () => {
-    setShowForm(!showForm); // Alterna la visibilidad del formulario
+  const handleToggleRegisterForm = () => {
+    setShowRegisterForm(!showRegisterForm); // Alterna la visibilidad del formulario
   };
 
-  const handleCloseForm = () => {
-    setShowForm(false); // Cierra el formulario
+  const handleCloseRegisterForm = () => {
+    setShowRegisterForm(false); // Cierra el formulario
+  };
+
+  const handleToggleLoginForm = () => {
+    setShowLoginForm(!showRegisterForm); // Alterna la visibilidad del formulario
+  };
+
+  const handleCloseLoginForm = () => {
+    setShowLoginForm(false); // Cierra el formulario
   };
 
   return (
@@ -24,22 +33,32 @@ const Navbar: React.FC = () => {
           <img className={styles.image} src="./images/UrgenciasYa.png" alt="Logo"/>
         </div>
         <div className={styles.links}>
-          <Button type="button" onClick={handleToggleForm} className={styles.navButton}>
+          <Button type="button" onClick={handleToggleLoginForm} className={styles.navButton}>
             Iniciar sesión
           </Button>
-          <Button type="button" onClick={handleToggleForm} className={styles.navButton}>
+          <Button type="button" onClick={handleToggleRegisterForm} className={styles.navButton}>
             Registrarse
           </Button>
         </div>
       </nav>
       {/* Mostrar el formulario cuando showForm sea true */}
-      {showForm && (
+      {showRegisterForm && (
         <div className={styles.modalContainer}>
-          <button className={styles.closeButton} onClick={handleCloseForm}>
+          <button className={styles.closeButton} onClick={handleCloseRegisterForm}>
             <CgCloseO/>
           </button>
           <div className={styles.formContainer}>
             <RegisterForm></RegisterForm>
+          </div>
+        </div>
+      )}
+      {showLoginForm && (
+        <div className={styles.modalContainer}>
+          <button className={styles.closeButton} onClick={handleCloseLoginForm}>
+            <CgCloseO/>
+          </button>
+          <div className={styles.formContainer}>
+            <h1>Hola</h1>
           </div>
         </div>
       )}
