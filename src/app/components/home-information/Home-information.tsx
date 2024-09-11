d8dc233b91f0ee0f523201c5f6f665d3ddea1c1b
Sebastian <sebastianroldangiraldo@gmail.com>
2024-09-11 15:39:39 -0500

    Update styles in components

--- src/app/components/home-information/Home-information.tsx
+++ src/app/components/home-information/Home-information.tsx
@@ -8,6 +8,7 @@ const HomeInformation: React.FC = () => {
             <div className={styles.containerLeft}>
                 <h1 className={styles.title}>Urgencias Ya</h1>
                 <p className={styles.description}>Al indicarnos tu <b>municipio actual</b> y <b>EPS</b>, realizaremos una búsqueda oportuna para que conozcas con certeza el centro de atención más cercano para acudir con tu urgencia. Recuerda, eres la prioridad.</p>
+                <p className={styles.admiration}>¡Tu <b>urgencia</b> es la prioridad!</p>
             </div>
             <div className={styles.containerRight}>
                 <SearchForm></SearchForm>
