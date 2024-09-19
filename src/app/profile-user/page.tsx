7df8f36eabc2dc7b5df6f1f6aae5efc63a12884b
sebas1913 <sebasosornicastrillon@gmail.com>
2024-09-19 11:22:36 -0500

    Update page profile user

--- src/app/profile-user/page.tsx
+++ src/app/profile-user/page.tsx
@@ -2,20 +2,62 @@
 import React from "react";
 import styles from "./profile-user.module.scss";
 import Button from "../../components/UI/button/Button";
+import { FaRegHeart, FaIdCard, FaRegEnvelope, FaPhoneAlt, FaUserMd } from "react-icons/fa";
+import { FaPenToSquare } from "react-icons/fa6";
 
-const Profile = () =>{
-    return(
-        <div className={styles.informationContainer}>
-            <div className={styles.containerLeft}>
-                <h1 className={styles.title}>Bienvenido</h1>
-                <h2 className={styles.nameUser}>Sebastián</h2>
-                <p className={styles.description}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequatur, amet laudantium? Delectus qui autem tempore similique deserunt ullam magni totam dicta voluptas repudiandae. Excepturi quidem, ipsam dicta fuga provident nobis voluptas exercitationem..</p>
-                <Button type="button" className={styles.button}>Realizar búsqueda</Button>
-            </div>
-            <div className={styles.containerRight}>
-                
-            </div>
 
+const Profile = () => {
+    return (
+        <div className={styles.profileContainer}>
+            <div className={styles.profileInformation}>
+                <div className={styles.information}>
+                    <div className={styles.welcome}>
+                        <h1 className={styles.profileTitle}>¡Hola, Diego!</h1>
+                        <div className={styles.welcomeInformation}>
+                            <p>Al auntenticarte tienes nuevas opciones de accesbilidad. Ahora, al darle click al botón, puedes realizar tu <b>búsqueda</b> de manera autmática.</p>
+                            <br />
+                            <p>¡También puedes participar de los <b>chats grupales </b>de cada hospital y solicitar un <b>turno</b>!</p>
+                        </div>
+                        <div className={styles.containerButton}>
+                            <Button className={styles.searchButton} type="button">Realizar búsqueda</Button>
+                        </div>
+                    </div>
+                    <div className={styles.tickets}>
+                        <h1 className={styles.ticketTitle}>Mis turnos</h1>
+                        {/* Tabla */}
+                    </div>
+                </div>
+                <div className={styles.profileData}>
+                    <div className={styles.dataHeader}>
+                    <FaUserMd className={styles.imageUser}/>
+
+                        <h2>Diego Guayabas</h2>
+                        <p>Usuario</p>
+                    </div>
+                    <div className={styles.data}>
+                        <div className={styles.iconInformation}>
+                            <FaRegHeart />
+                            <p className={styles.description}>Sura</p>
+                        </div>
+                        <div className={styles.iconInformation}>
+                            <FaIdCard />
+                            <p className={styles.description}>100992012</p>
+                        </div>
+                        <div className={styles.iconInformation}>
+                            <FaRegEnvelope />
+                            <p className={styles.description}>diego@gmail.com</p>
+                        </div>
+                        <div className={styles.iconInformation}>
+                            <FaPhoneAlt />
+                            <p className={styles.description}>3218898812</p>
+                        </div>
+                        <div className={styles.iconInformation}>
+                            <FaPenToSquare />
+                            <p className={styles.description}>Editar</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
