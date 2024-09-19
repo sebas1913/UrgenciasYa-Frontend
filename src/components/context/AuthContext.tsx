58f1c998342a29c83cd45ee68bc3730599da67d3
sebas1913 <sebasosornicastrillon@gmail.com>
2024-09-19 16:47:33 -0500

    Refactoring

--- src/components/context/AuthContext.tsx
+++ src/components/context/AuthContext.tsx
@@ -1,16 +1,15 @@
-// src/context/AuthContext.tsx
-// src/context/AuthContext.tsx
-'use client';
+"use client";
 import { IUser } from '@/interfaces/IUser';
 import { useRouter } from 'next/navigation';
-import React, { createContext, useContext, useState } from 'react';
+import React, { createContext, useContext, useState, useEffect } from 'react';
+import cookie from 'cookie';
 
 interface AuthContextType {
     isAuthenticated: boolean;
     user: IUser | null;
     login: (token: string, userInfo: IUser) => void;
     logout: () => void;
-};
+}
 
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
@@ -18,19 +17,39 @@ export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children
     const router = useRouter();
     const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
     const [user, setUser] = useState<IUser | null>(null);
+    const [authChecked, setAuthChecked] = useState<boolean>(false); 
+
+    useEffect(() => {
+        const cookies = cookie.parse(document.cookie || '');
+        const token = cookies.auth;
+
+        if (token) {
+            const userInfo = JSON.parse(localStorage.getItem('userInfo') || '{}');
+            setIsAuthenticated(true);
+            setUser(userInfo);
+        }
+        setAuthChecked(true);  // La autenticación ha sido verificada
+    }, []);
 
     const login = (token: string, userInfo: IUser) => {
         document.cookie = `auth=${token}; path=/;`;
+        localStorage.setItem('userInfo', JSON.stringify(userInfo)); // Guardamos la info del usuario localmente
         setIsAuthenticated(true);
         setUser(userInfo);
     };
 
     const logout = () => {
         document.cookie = `auth=; Max-Age=0; path=/;`;
+        localStorage.removeItem('userInfo'); // Eliminamos la info del usuario
         setIsAuthenticated(false);
         setUser(null);
         router.replace('/');
     };
+
+    if (!authChecked) {
+        // Mientras verificamos la autenticación, no renderizamos nada
+        return null;
+    }
 
     return (
         <AuthContext.Provider value={{ isAuthenticated, user, login, logout }}>
