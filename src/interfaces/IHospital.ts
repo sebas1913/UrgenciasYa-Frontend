56e0d104c34fa885c0c0b94c4ce00f2eb5c1035b
Sebastian <sebastianroldangiraldo@gmail.com>
2024-09-19 08:10:52 -0500

    Update search results page components

---
+++ src/interfaces/IHospital.ts
@@ -0,0 +1,8 @@
+export interface IHospital {
+    name: string;           
+    phone_number: string;   
+    howtogetthere?: string;  
+    rating: string | number;        
+    url_image: string; 
+    nameTown : string;   
+};
