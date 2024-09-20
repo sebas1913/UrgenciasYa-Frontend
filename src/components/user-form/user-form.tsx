73a57fd49cb7d41a52164f2e29d1f401136fab9e
sebas1913 <sebasosornicastrillon@gmail.com>
2024-09-20 16:44:36 -0500

    Create user update form

--- src/components/user-form/user-form.tsx
+++ src/components/user-form/user-form.tsx
@@ -1,23 +1,132 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Button from "../UI/button/Button";
 import Input from "../UI/input/Input";
 import styles from "./user-form.module.scss";
 import Form from "../UI/form/Form";
 import Label from "../UI/label/Label";
+import Select from "../UI/select/Select";
+import { useAuth } from "../context/AuthContext";
 
-const UserForm: React.FC = () => {
+const UpdateUserForm: React.FC = () => {
+    const { login } = useAuth();
     const [isFormVisible, setIsFormVisible] = useState(true);
     const [selectedName, setSelectedName] = useState('');
-    const [errorMessage, setErrorMessage] = useState(''); // Estado para el mensaje de error
+    const [selectedEmail, setSelectedEmail] = useState('');
+    const [selectedEps, setSelectedEps] = useState('');
+    const [eps, setEps] = useState<{ label: string, value: string }[]>([]);
+    const [selectedDocument, setSelectedDocument] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
 
-    const onSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        console.log('Enviado :)');
-    }
+    useEffect(() => {
+        const fetchUserData = async () => {
+            try {
+                const responseID = localStorage.getItem('userID');
+                if (responseID) {
+                    const userID = JSON.parse(responseID);
+                    const userResponse = await fetch(`http://localhost:8080/${userID.id}`);
+                    if (userResponse.ok) {
+                        const userData = await userResponse.json();
+                        setSelectedName(userData.name);
+                        setSelectedEmail(userData.email);
+                        setSelectedEps(userData.eps);
+                        setSelectedDocument(userData.document);
+                    } else {
+                        throw new Error(`Error al obtener datos del usuario: ${userResponse.statusText}`);
+                    }
+                }
+            } catch (error) {
+                console.error("Error fetching user data:", error);
+                setErrorMessage('Error al obtener datos del usuario.');
+            }
+        };
+
+        fetchUserData();
+        fetchEps();
+    }, []);
+
+    const fetchEps = async () => {
+        try {
+            const response = await fetch("http://localhost:8080/api/v1/eps");
+            const data = await response.json();
+            const epsOptions = data.map((element: any) => ({
+                label: element.name,
+                value: element.name
+            }));
+
+            const defaultOption = { label: 'Ingresa una opción', value: '' };
+            const optionsWithDefault = [defaultOption, ...epsOptions];
+
+            setEps(optionsWithDefault);
+        } catch (error) {
+            console.error("Error fetching EPS:", error);
+        }
+    };
 
     const handleChangeName = (event: React.ChangeEvent<HTMLInputElement>) => {
         setSelectedName(event.target.value);
+    };
+
+    const handleChangeEmail = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setSelectedEmail(event.target.value);
+    };
+
+    const handleChangeEps = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        setSelectedEps(event.target.value);
+    };
+
+    const handleChangeDocument = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setSelectedDocument(event.target.value);
+    };
+
+    const onSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+
+        if (!selectedName || !selectedEps || !selectedEmail || !selectedDocument) {
+            alert('Por favor, completa todos los campos.');
+            return;
+        }
+
+        const responseID = localStorage.getItem('userID');
+        if (responseID) {
+            const userID = JSON.parse(responseID);
+
+            try {
+                const response = await fetch(`http://localhost:8080/${userID.id}`, {
+                    method: 'PUT',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({
+                        name: selectedName,
+                        document: selectedDocument,
+                        eps: selectedEps,
+                        email: selectedEmail,
+                    }),
+                });
+
+                const data = await response.json(); // Solo lo llamamos una vez aquí
+
+                if (response.ok) {
+                    setIsFormVisible(false);
+                    const newToken = data.token;
+
+                    if (newToken) {
+                        login(newToken, { id: userID.id });
+                        setTimeout(() => {
+                            window.location.reload();
+                        }, 1500);
+                    }
+                } else {
+                    throw new Error(`Error en la actualización: ${data.message || response.statusText}`);
+                }
+
+                console.log("Actualización exitosa:", data);
+
+            } catch (error) {
+                console.error("Error en la solicitud:", error);
+                setErrorMessage('Error al actualizar la información.');
+            }
+        }
     };
 
     return (
@@ -41,6 +150,51 @@ const UserForm: React.FC = () => {
                             />
                         </div>
 
+                        <div className={styles.formElement}>
+                            <Label
+                                htmlFor="document"
+                                className={styles.label}
+                            >Ingresa tu documento:</Label>
+                            <Input
+                                id='document'
+                                type='number'
+                                name='document'
+                                value={selectedDocument}
+                                onChange={handleChangeDocument}
+                                className={styles.input}
+                            />
+                        </div>
+
+                        <div className={styles.formElement}>
+                            <Label
+                                htmlFor="eps"
+                                className={styles.label}
+                            >Ingresa tu EPS afiliada:</Label>
+                            <Select
+                                id="eps"
+                                options={eps}
+                                value={selectedEps}
+                                onChange={handleChangeEps}
+                                className={styles.selectEPS}
+                                disabled={false}
+                            />
+                        </div>
+
+                        <div className={styles.formElement}>
+                            <Label
+                                htmlFor="email"
+                                className={styles.label}
+                            >Ingresa tu correo electrónico:</Label>
+                            <Input
+                                id='email'
+                                type='email'
+                                name='email'
+                                value={selectedEmail}
+                                onChange={handleChangeEmail}
+                                className={styles.input}
+                            />
+                        </div>
+
                         <div>
                             <Button
                                 type='submit'
@@ -51,15 +205,13 @@ const UserForm: React.FC = () => {
                 </>
             ) : (
                 errorMessage ? (
-                    <p className={styles.errorMessage}>{errorMessage} Error</p>
+                    <p className={styles.errorMessage}>{errorMessage}</p>
                 ) : (
-                    <p className={styles.successMessage}>¡Tu info ha sido actualizada!</p>
+                    <p className={styles.successMessage}>¡Tu información ha sido actualizada!</p>
                 )
             )}
         </div>
-
     );
-}
+};
 
-
-export default UserForm;
+export default UpdateUserForm;
